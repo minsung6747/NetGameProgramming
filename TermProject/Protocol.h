c777fd1f34334bc837b47d55b010594a5698f677
Sang Min <alstkdalsdl@naver.com>
2023-12-01 14:42:50 +0900

    modified protocol

--- TermProject/Protocol.h
+++ TermProject/Protocol.h
@@ -4,6 +4,8 @@ constexpr char SC_PLAYER_MOVE = 0;
 constexpr char SC_KEY_INPUT = 1;
 constexpr char SC_PLAYER_ROTATE = 2;
 constexpr char SC_SEND_PLAYER = 3;
+extern char  PACKET_GEMSTONE = 4;
+
 
 // 예시로 구조체 정의
 
@@ -27,4 +29,9 @@ struct ROTATE_PACKET {
 struct SEND_PLAYER {
 	char type;
 	int id;
+};
+
+struct GemStonePacket {
+	char cType;
+	float fX, fY, fZ;
 };
